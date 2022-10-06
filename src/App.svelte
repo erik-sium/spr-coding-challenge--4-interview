f54fbfff20a3a29f44d0f2e0c39fa48f1165f059
erik-sium <eriksingh15@gmail.com>
2022-10-07 01:28:31 +0200

    Added button to open left pane that will contain the node navigation history interface

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
 <script>
+	import NodeNavigationHistory from "./components/NodeNavigationHistoryPane.svelte";
+
 	export let name;
+
+	let showNodeNavigationHistoryPane = false
+	const openNodeNavigationHistory = () => {
+		showNodeNavigationHistoryPane = !showNodeNavigationHistoryPane
+	}
 </script>
 
 <main>
 	<h1>SAPORO Coding Challenge by {name}</h1>
 	<p>Task: Recreate node detail history</p>
 	<p>Implemented with Svelte.js</p>
+
+	<div id="button_container">
+		<button on:click={() => openNodeNavigationHistory()}>
+			Click me to start!<slot></slot>
+		</button>
+	</div>
+
+	{#if showNodeNavigationHistoryPane}
+		<NodeNavigationHistory />
+	{/if}
 </main>
 
 <style>
@@ -27,5 +44,11 @@
 		main {
 			max-width: none;
 		}
+	}
+
+	#button_container {		
+		margin-top: 100px;
+		position: absolute;
+		left: 200px;
 	}
 </style>
---
+++ src/components/NodeNavigationHistoryPane.svelte
@@ -0,0 +1,16 @@
+<div class="node-navigation-history-pane">
+  
+</div>
+
+<style>
+  .node-navigation-history-pane {
+    background-color: lightgray;
+    position: absolute;
+    right: 0;
+
+    width: 58%;
+    min-height: 100%;
+    float: right;
+  }
+
+</style>
