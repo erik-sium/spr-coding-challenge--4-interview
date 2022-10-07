83c1b6693068ac362b236b6880b7ba74bcefc336
erik-sium <eriksingh15@gmail.com>
2022-10-07 03:25:20 +0200

    Markup and style changes to create the layout of the solution and correctly place the elements of the solution

--- src/components/NodeNavigationHistoryPane.svelte
+++ src/components/NodeNavigationHistoryPane.svelte
@@ -1,16 +1,91 @@
+<script>
+	const backButtonPress = () => {
+		
+	}
+</script>
+
 <div class="node-navigation-history-pane">
+
+  <div id="back-button-and-history-table">
+    <button id="back-button" on:click={() => backButtonPress()}>
+      Back
+    </button>
+
+    <table id="history-table">
+      <td>
+        a
+      </td>
+      <td>
+        b
+      </td>
+    </table> 
+  </div>
+
+  <div id="detail">
+    Detail:
+  </div>
+
+  <div id="items-table">
+    <table>
+      <tr>
+        ciao
+      </tr>
+      <tr>
+        miao
+      </tr>
+      <tr>
+        azzo
+      </tr>
+    </table> 
+  </div>
   
+
 </div>
 
 <style>
+
+  table, td, tr, th {
+
+    border-collapse: collapse;
+    border: 1px solid;
+  }
+
   .node-navigation-history-pane {
-    background-color: lightgray;
+    background-color: lightcyan;
     position: absolute;
     right: 0;
 
     width: 58%;
-    min-height: 100%;
     float: right;
+  }
+
+  .node-navigation-history-pane > * {
+    background-color: lightgray;
+    width: auto;
+    margin: 25px;
+    padding: 10px;
+  }
+ 
+  #back-button-and-history-table {
+    text-align: left;
+  }
+
+  #back-button {
+    display: inline-block;
+  }
+
+  #history-table {
+    display: inline-block;
+    margin-left: 25px;
+    vertical-align:middle;
+  }
+
+  #detail {
+    text-align: left;
+  }
+
+  #items-table > table {
+    width: 100%;
   }
 
 </style>
